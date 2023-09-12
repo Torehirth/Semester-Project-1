525b87817fa13c230bb1a5cb8a74744394121bac
Tore Mæland Hirth <torehirth@gmail.com>
2023-09-12 18:47:14 +0200

    moved JS code on Visit page to JS-file

--- script.js
+++ script.js
@@ -1,9 +1,50 @@
-document.addEventListener('DOMContentLoaded', function() {
+// IOS positioned fixed
+document.addEventListener("DOMContentLoaded", function () {
   // Check if the user agent is iOS
   const isiOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
 
   if (isiOS) {
-      // Add a class to the container for iOS devices
-      document.querySelector('.container').classList.add('ios-background-fixed');
+    // Add a class to the container for iOS devices
+    document.querySelector(".container").classList.add("ios-background-fixed");
   }
 });
+
+// Google Maps Home page
+function initMap() {
+  const myLatLng = {
+    lat: 59.91725540161133,
+    lng: 10.727352142333984,
+  };
+  const map = new google.maps.Map(document.getElementById("gmp-map"), {
+    zoom: 10,
+    center: myLatLng,
+    fullscreenControl: false,
+    zoomControl: true,
+    streetViewControl: false,
+  });
+  new google.maps.Marker({
+    position: myLatLng,
+    map,
+    title: "My location",
+  });
+}
+
+// Google Maps Visit Us page
+function initMap() {
+  const myLatLng = {
+    lat: 59.91725540161133,
+    lng: 10.727352142333984,
+  };
+  const map = new google.maps.Map(document.getElementById("gmp-map2"), {
+    zoom: 10,
+    center: myLatLng,
+    fullscreenControl: false,
+    zoomControl: true,
+    streetViewControl: false,
+  });
+  new google.maps.Marker({
+    position: myLatLng,
+    map,
+    title: "My location",
+  });
+}
