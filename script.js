aaec6e0e1b5dcf20a0eb0616d503597718ec0e46
Tore Mæland Hirth <torehirth@gmail.com>
2023-09-12 19:31:21 +0200

    changed bckgr img class from inside main-element to body-element

--- script.js
+++ script.js
@@ -8,23 +8,3 @@ document.addEventListener("DOMContentLoaded", function () {
     document.querySelector(".container").classList.add("ios-background-fixed");
   }
 });
-
-// Google Maps Visit Us page
-function initMap() {
-  const myLatLng = {
-    lat: 59.91725540161133,
-    lng: 10.727352142333984,
-  };
-  const map = new google.maps.Map(document.getElementById("gmp-map2"), {
-    zoom: 10,
-    center: myLatLng,
-    fullscreenControl: false,
-    zoomControl: true,
-    streetViewControl: false,
-  });
-  new google.maps.Marker({
-    position: myLatLng,
-    map,
-    title: "My location",
-  });
-}
