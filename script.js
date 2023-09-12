d3b159c68e20509d973ce9750fa388d293525cb2
Tore Mæland Hirth <torehirth@gmail.com>
2023-09-12 18:40:32 +0200

    add js file and moved code

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // Check if the user agent is iOS
+  const isiOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
+
+  if (isiOS) {
+      // Add a class to the container for iOS devices
+      document.querySelector('.container').classList.add('ios-background-fixed');
+  }
+});
